5b6ab7f77d2408d4d69d3bf6bc4884e62cd56a6f
Grig Alex <alexengrigdev@gmail.com>
2019-12-20 21:26:08 +0300

    Add 1/8 solution

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // put your code here
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
     }
 }
