1b197a60ef4c486d823d16299e3e9e196ef0ac3e
Grig Alex <alexengrigdev@gmail.com>
2019-12-20 21:33:52 +0300

    Add 2/8 solution

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -6,8 +6,21 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println(a + b);
+        for (; ; ) {
+            String line = scanner.nextLine();
+            if ("/exit".equals(line)) {
+                break;
+            }
+            String[] numbers = line.trim().split("\\s+");
+            if (numbers.length == 1 && !numbers[0].isBlank()) {
+                int a = Integer.parseInt(numbers[0]);
+                System.out.println(a);
+            } else if (numbers.length == 2) {
+                int a = Integer.parseInt(numbers[0]);
+                int b = Integer.parseInt(numbers[1]);
+                System.out.println(a + b);
+            }
+        }
+        System.out.println("Bye!");
     }
 }
--- Smart Calculator/task/test/Test.java
+++ Smart Calculator/task/test/Test.java
@@ -1,52 +1,31 @@
 import calculator.Main;
 import org.hyperskill.hstest.v6.stage.BaseStageTest;
-import org.hyperskill.hstest.v6.testcase.CheckResult;
-import org.hyperskill.hstest.v6.testcase.TestCase;
+import org.hyperskill.hstest.v6.testcase.PredefinedIOTestCase;
 
 import java.util.List;
-import java.util.Objects;
 
-public class Test extends BaseStageTest<String> {
+public class Test extends BaseStageTest {
 
     public Test() throws Exception {
         super(Main.class);
     }
 
-    private String solveStage(String input) {
-        String[] nums = input.split("\\s+");
-        int a = Integer.parseInt(nums[0]);
-        int b = Integer.parseInt(nums[1]);
-        return Objects.toString(a + b);
-    }
-
     @Override
-    public List<TestCase<String>> generate() {
-        List<TestCase<String>> tests = List.of(
-            new TestCase<String>().setInput("0 1"),
-            new TestCase<String>().setInput("1 0"),
-            new TestCase<String>().setInput("2 3"),
-            new TestCase<String>().setInput("100 123"),
-            new TestCase<String>().setInput("-1 5"),
-            new TestCase<String>().setInput("5 -2"),
-            new TestCase<String>().setInput("-300 -400")
+    public List<PredefinedIOTestCase> generate() {
+        return List.of(
+                new PredefinedIOTestCase(
+                        "/exit",
+                        "Bye!"
+                ),
+                new PredefinedIOTestCase(
+                        "17 9\n-2 5\n\n7\n/exit",
+                        "26\n3\n7\nBye!"
+                ),
+                new PredefinedIOTestCase(
+                        "100 200\n500\n300 400\n200\n\n\n-500\n/exit",
+                        "300\n500\n700\n200\n-500\nBye!"
+                )
         );
-
-        for (TestCase<String> test : tests) {
-            test.setAttach(solveStage(test.getInput()));
-        }
-
-        return tests;
     }
 
-    @Override
-    public CheckResult check(String reply, String clue) {
-        try {
-            int actual = Integer.parseInt(reply.trim());
-            int expected = Integer.parseInt(clue.trim());
-            return new CheckResult(actual == expected);
-        }
-        catch (Exception ex) {
-            return new CheckResult(false, "Can't check the answer");
-        }
-    }
 }
