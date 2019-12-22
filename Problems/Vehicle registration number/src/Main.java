57a53fa80f462a9dcf3710f0cfc773bb090ec7cb
Grig Alex <alexengrigdev@gmail.com>
2019-12-22 14:19:06 +0300

    Add "Vehicle registration number" problem solution

---
+++ Problems/Vehicle registration number/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String regNum = scanner.nextLine(); // a valid or invalid registration number
+
+        System.out.println(regNum.matches("[ABEKMHOPCTYX]\\d{3}[ABEKMHOPCTYX]{2}"));
+    }
+}
