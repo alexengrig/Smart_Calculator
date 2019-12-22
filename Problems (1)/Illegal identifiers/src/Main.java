432877423b4183220c77e0a6e31a3f1b18edd57c
Grig Alex <alexengrigdev@gmail.com>
2019-12-22 18:47:54 +0300

    Add "Illegal identifiers" problem solution

---
+++ Problems (1)/Illegal identifiers/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfVariables = Integer.parseInt(scanner.nextLine());
+
+        String regex = "(_[a-zA-Z][\\w_]*|[a-zA-Z][\\w_]*)";
+
+        for (int i = 0; i < numberOfVariables; i++) {
+            String identifier = scanner.nextLine();
+            if (!identifier.matches(regex)) {
+                System.out.println(identifier);
+            }
+        }
+    }
+}
