b3b0b094c4e77a8fe54f1f7e816159b7cb1b7e78
Grig Alex <alexengrigdev@gmail.com>
2020-01-04 13:46:34 +0300

    Add "Difference between two doubles" problem

---
+++ Problems (12)/Difference between two doubles/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double first = scanner.nextDouble();
+        double second = scanner.nextDouble();
+        System.out.println(second - first);
+    }
+}
