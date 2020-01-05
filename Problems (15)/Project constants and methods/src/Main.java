57dee0fdaabd14bb03bb4c0d4dba92e9efae7c21
Grig Alex <alexengrigdev@gmail.com>
2020-01-05 23:55:40 +0300

    Add "Project constants and methods" problem

---
+++ Problems (15)/Project constants and methods/src/Main.java
@@ -0,0 +1,6 @@
+public static void main(String args[]) {
+    System.out.println(ConstantsAndUtilities.A_CONSTANT_TTT);
+    System.out.println(ConstantsAndUtilities.B_CONSTANT_QQQ);
+    System.out.println(ConstantsAndUtilities.getMagicString());
+    System.out.println(ConstantsAndUtilities.convertStringToAnotherString("aa"));
+}
