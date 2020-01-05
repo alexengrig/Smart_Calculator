418514e81340454fe3cc8dc0f49f34f5f57f6433
Grig Alex <alexengrigdev@gmail.com>
2020-01-05 22:36:50 +0300

    Add "The product of numbers from a to b" problem

---
+++ Problems (13)/The product of numbers from a to b/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        long product = 1;
+        for (int i = a; i < b; i++) {
+            product *= i;
+        }
+        System.out.println(product);
+    }
+}
