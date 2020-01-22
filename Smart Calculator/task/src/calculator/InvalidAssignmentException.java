15dc6692d66001769e3237120b6a86b125b6f684
Grig Alex <alexengrigdev@gmail.com>
2020-01-22 19:53:27 +0300

    Remove unused classes

--- Smart Calculator/task/src/calculator/InvalidAssignmentException.java
+++
@@ -1,8 +0,0 @@
-package calculator;
-
-public class InvalidAssignmentException extends RuntimeException {
-    @Override
-    public String toString() {
-        return "Invalid assignment";
-    }
-}
--- Smart Calculator/task/src/calculator/InvalidIdentifierException.java
+++
@@ -1,8 +0,0 @@
-package calculator;
-
-public class InvalidIdentifierException extends RuntimeException {
-    @Override
-    public String toString() {
-        return "Invalid identifier";
-    }
-}
